b95f14f32dde847e8d3ffed7e5683ea4d003d843
TakuKobayashi <keep_slimbody@yahoo.co.jp>
2019-06-18 17:29:24 +0900

    各種必要なものを入れてみた

---
+++ app/src/main/java/net/taptappun/taku/kobayashi/somethingscanner/ScannerActivity.kt
@@ -0,0 +1,12 @@
+package net.taptappun.taku.kobayashi.somethingscanner
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ScannerActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
