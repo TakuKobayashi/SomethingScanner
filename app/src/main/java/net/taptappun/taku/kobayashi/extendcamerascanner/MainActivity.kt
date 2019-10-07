ba92b4a72d70d5eaf97e210964bdf6729af328d9
TakuKobayashi <keep_slimbody@yahoo.co.jp>
2019-10-08 02:11:18 +0900

    通知関係を色々と０変えてみた

--- app/src/main/java/net/taptappun/taku/kobayashi/extendcamerascanner/MainActivity.kt
+++ app/src/main/java/net/taptappun/taku/kobayashi/extendcamerascanner/MainActivity.kt
@@ -3,12 +3,13 @@ package net.taptappun.taku.kobayashi.extendcamerascanner
 import android.content.Intent
 import android.os.Bundle
 import android.util.Log
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.gc.materialdesign.views.ButtonRectangle
 import com.google.firebase.analytics.FirebaseAnalytics
 import com.google.firebase.iid.FirebaseInstanceId
 import net.taptappun.taku.kobayashi.runtimepermissionchecker.RuntimePermissionChecker
-import com.allyants.notifyme.NotifyMe
+import io.karn.notify.Notify
 
 class MainActivity : AppCompatActivity() {
     private val PERMISSION_REQUEST_CODE = 1
@@ -40,27 +41,36 @@ class MainActivity : AppCompatActivity() {
 
         val analyticsHookButton = findViewById<ButtonRectangle>(R.id.analyticsHookButton)
         analyticsHookButton.setOnClickListener({ v ->
+            Log.d(Const.TAG, "Button Press")
             val instanceId = FirebaseInstanceId.getInstance().instanceId
             instanceId.addOnCompleteListener { task ->
-                val notifyMe = NotifyMe.Builder(applicationContext)
-                if (!task.isSuccessful) {
+                Log.d(Const.TAG, "Log Event Success")
+                var titleText = "";
+                var contentText = "";
+                if (task.isSuccessful) {
                     Log.d(Const.TAG, "FCM Generate InstanceId:" + task.result?.token)
-                    notifyMe.title("Success");
-                    notifyMe.content("FCM Generate InstanceId:" + task.result?.token);
+                    titleText = "Success"
+                    contentText = "FCM Generate InstanceId:" + task.result?.token
                 }else{
                     Log.d(Const.TAG, "FCM Generate InstanceId Fail")
-                    notifyMe.title("Fail");
-                    notifyMe.content("FCM Generate InstanceId Fail");
+                    titleText = "Fail"
+                    contentText = "FCM Generate InstanceId Fail"
                 }
-                notifyMe.build();
+                val notifyMe = Notify.with(applicationContext).content {
+                    title = titleText
+                    text = contentText
+                }.show()
+                Toast.makeText(this, "Success:" + task.isSuccessful + " token:" + task.result?.token, Toast.LENGTH_LONG).show()
             }
             val bundle = Bundle()
             bundle.putString(FirebaseAnalytics.Param.ITEM_ID, R.id.analyticsHookButton.toString())
             bundle.putString(FirebaseAnalytics.Param.ITEM_NAME, "analyticsHookButton")
             bundle.putString(FirebaseAnalytics.Param.CONTENT_TYPE, "button")
             analytics.logEvent(FirebaseAnalytics.Event.SELECT_CONTENT, bundle)
+            Log.d(Const.TAG, "Log Event Success")
 
         })
+        Log.d(Const.TAG, "analytics" + analyticsHookButton)
 
         RuntimePermissionChecker.requestAllPermissions(this, PERMISSION_REQUEST_CODE)
     }
